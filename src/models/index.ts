aeadafbe9b913233164d6944bd9fbd389efa9f60
zhangxwhope <zhangxwhope@163.com>
2021-03-10 10:34:34 +0800

    登录

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,25 +1,24 @@
-
 import { Effect, ImmerReducer } from 'umi';
-import { getMyInfo, login } from '@/services/api/login'
+import { getMyInfo, login } from '@/services/api/login';
 
 export interface IndexModelState {
   userInfo: any; // 用户登录信息
   loginState: boolean; // 是否为登录状态
-  personalInfo: any // 个人信息
+  personalInfo: any; // 个人信息
 }
 
 export interface IndexModelType {
   namespace: string;
   state: IndexModelState;
   effects: {
-    query: Effect,
-    login: Effect
-  },
+    query: Effect;
+    login: Effect;
+  };
   reducers: {
     updateUser: ImmerReducer<IndexModelState>;
     updateLoginState: ImmerReducer<IndexModelState>;
     updatePersonalInfo: ImmerReducer<IndexModelState>;
-  }
+  };
 }
 
 const IndexModel: IndexModelType = {
@@ -27,31 +26,39 @@ const IndexModel: IndexModelType = {
   state: {
     userInfo: {}, // 用户登录信息
     loginState: true, // 是否为登录状态
-    personalInfo: {} // 个人信息
+    personalInfo: {}, // 个人信息
   },
   effects: {
     *query({ payload }, { call, put }) {
-      const res = yield call(getMyInfo, payload)
+      const res = yield call(getMyInfo, payload);
       // console.log(res, 'yield')
       // yield put({ type: 'updatePersonalInfo', res })
-      yield put({ type: 'updateLoginState', payload: false })
-        
+      yield put({ type: 'updateLoginState', payload: false });
     },
     *login({ payload }, { call, put }) {
-      const res = yield call(login, payload)
-    }
+      const res = yield call(login, payload);
+      if (res.code === 'T') {
+        const { token, userInfo } = res.data || {};
+        localStorage.setItem('token', token);
+        yield put({
+          type: 'updateUser',
+          payload: userInfo,
+        });
+        yield put({ type: 'updateLoginState', payload: true });
+      }
+    },
   },
   reducers: {
     updateUser(state: any, { payload }: any) {
-      return { ...state, userInfo: payload }
+      return { ...state, userInfo: payload };
     },
     updateLoginState(state: any, { payload }: any) {
-      return { ...state, loginState: payload }
+      return { ...state, loginState: payload };
     },
     updatePersonalInfo(state: any, { payload }: any) {
-      return { ...state, personalInfo: payload }
-    }
-  }
-}
+      return { ...state, personalInfo: payload };
+    },
+  },
+};
 
-export default IndexModel
+export default IndexModel;
